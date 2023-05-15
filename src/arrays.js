3cc37df55df5175ca35015c02dc18943f7051481
1994Supun <pathiranahansaka@gmail.com>
2023-05-16 10:58:01 +1200

    add more

---
+++ src/arrays.js
@@ -0,0 +1,54 @@
+// make a function that sorts an array and delete any duplicates.
+
+//NOTE - duplicatesArray([1,2,3,4,5,]) -> [1,2,3,4,5]
+
+//Delete duplicating arrays new fucntion 
+
+function duplicatesArray(arr) {
+    const newArray = []
+    
+    let currentArray = arr[0]
+
+    newArray.push(currentArray)
+    for(let i =0; i< arr.length; i++){
+        if(arr[i] !== currentArray){
+            currentArray = arr[i]
+            newArray.push(currentArray)
+        }
+    }
+    return newArray
+}
+
+// Develop a function that counts number  (0)/s in 20 array. 
+
+//NOTE - twoArrays([[0,0],[0,1]]) -> 3
+
+function twoArrays(arr) {
+    let count = 0 
+
+    for(let i = 0; i < arr.length; i++) {
+        for (let j =0; j < arr[i].length; j++){
+
+        }
+    
+    
+
+   
+
+
+{
+
+
+
+
+// Write a function that returns the largest even number in an array.
+//evenLargestNumber([1,10,17]) -> 10
+//evenLargestNumber([1,3,5,7,9]) -> "No even number"
+
+function evenLargestNumber(arr){
+
+
+
+
+
+}
